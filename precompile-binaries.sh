3be2e2486384b1ad6f765e0ca93d81da18cc309a
Brian McKenna <brian@precog.com>
2012-11-06 13:25:02 -0700

    Add deletion of hopefully unneeded parts of GHC

--- precompile-binaries.sh
+++ precompile-binaries.sh
@@ -11,23 +11,40 @@ cd ghc-7.4.1/
 make install
 cd ..
 
+# GHC space trimming (risky business)
+
+# Remove haddock and hpc - no docs or coverage
+rm $HOME/ghc/bin/haddock*
+rm $HOME/ghc/lib/ghc-7.4.1/haddock
+rm -r $HOME/ghc/lib/ghc-7.4.1/html
+rm -r $HOME/ghc/lib/ghc-7.4.1/latex
+rm $HOME/ghc/bin/hp*
+rm -r $HOME/ghc/lib/ghc-7.4.1/hp*
+rm -r $HOME/ghc/lib/ghc-7.4.1/package.conf.d/hpc-0.5.1.1-*
+rm -r $HOME/ghc/lib/ghc-7.4.1/ghc-7.4.1
+rm -r $HOME/ghc/lib/ghc-7.4.1/package.conf.d/ghc-7.4.1-*
+rm -r $HOME/ghc/lib/ghc-7.4.1/package.conf.d/package.cache
+echo "" > $HOME/ghc/lib/ghc-7.4.1/ghc-usage.txt
+echo "" > $HOME/ghc/lib/ghc-7.4.1/ghci-usage.txt
+
+# Remove duplicate libs
+find $HOME/ghc/lib -name "*_p.a" -delete
+find $HOME/ghc/lib -name "*.p_hi" -delete
+find $HOME/ghc/lib -name "*.dyn_hi" -delete
+find $HOME/ghc/lib -name "*HS*.so" -delete
+find $HOME/ghc/lib -name "*HS*.o" -delete
+find $HOME/ghc/lib -name "*_debug.a" -delete
+
+# Don't need man or doc
+rm -rf $HOME/ghc/share
+
+# Strip binaries
+strip --strip-unneeded $HOME/ghc/lib/ghc-7.4.1/{run,}ghc
+
 # ldconfig for linker hack
 $HOME/ghc/bin/ghc-pkg describe base > base.package.conf
 sed -i "s/ld-options:/ld-options:\ -L\/app\/usr\/lib/" base.package.conf
 $HOME/ghc/bin/ghc-pkg update base.package.conf
-
-# haskell-platform (disabled because it requires opengl)
-#curl --silent http://lambda.haskell.org/platform/download/2011.4.0.0/haskell-platform-2011.4.0.0.tar.gz|tar xz
-#cd haskell-platform-2011.4.0.0/
-
-# cabal
-curl --silent http://www.haskell.org/cabal/release/cabal-1.14.0/Cabal-1.14.0.tar.gz|tar xz
-cd Cabal-1.14.0/
-$HOME/ghc/bin/ghc --make Setup
-./Setup configure --user --with-ghc=$HOME/ghc/bin/ghc
-./Setup build
-./Setup install
-cd ..
 
 # cabal-install
 curl --silent http://www.haskell.org/cabal/release/cabal-install-0.14.0/cabal-install-0.14.0.tar.gz|tar xz
